df4cb9216f04fc27e0e18d4ee969cce3fc4fe716
Unknown <franizus@live.com>
2018-04-16 08:58:25 -0500

    agregado logs y cambio de variable texto

--- app/src/main/java/com/example/francisco/androidejemplo/MainActivity.kt
+++ app/src/main/java/com/example/francisco/androidejemplo/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.francisco.androidejemplo
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        texto_central.text = "Adios"
+        var valorTextoCentral: CharSequence = texto_central.text
+        Log.i("clase", "El texto que se muestra es ${texto_central.text} $valorTextoCentral")
+
+        setSupportActionBar(toolbar)
+        Log.e("clase", "Esto es un error")
+        Log.w("clase", "Esto es un warning")
+        Log.d("clase", "Esto es un debug")
+        Log.i("clase", "Esto es un info")
+        Log.v("clase", "Esto es un verbose")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
