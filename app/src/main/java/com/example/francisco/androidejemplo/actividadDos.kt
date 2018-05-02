b2f028fc062ee5863c8d20faab326490a05f5d06
Unknown <franizus@live.com>
2018-05-02 10:09:11 -0500

    added intent enviar correo

--- app/src/main/java/com/example/francisco/androidejemplo/actividadDos.kt
+++ app/src/main/java/com/example/francisco/androidejemplo/actividadDos.kt
@@ -18,10 +18,25 @@ class actividadDos : AppCompatActivity() {
         boton_dos.setOnClickListener { v: View ->
             irAActividadTres()
         }
+
+        boton_enviar_correo.setOnClickListener { v: View ->
+            enviarCorreo()
+        }
     }
 
     fun irAActividadTres() {
         val intent = Intent(this, ActividadTres::class.java)
         startActivity(intent)
     }
+
+    fun enviarCorreo() {
+        val addresses = arrayOf("direccion@uno.com", "direccion@dos.com")
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/html"
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses)
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Sujeto")
+        intent.putExtra(Intent.EXTRA_TEXT, "Texto.")
+        startActivity(intent)
+    }
+
 }
