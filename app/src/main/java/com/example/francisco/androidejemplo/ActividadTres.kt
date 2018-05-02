567a899cb120be4e3133f1a007716cf11a80a04b
Unknown <franizus@live.com>
2018-05-02 10:25:05 -0500

    added share information between apps

--- app/src/main/java/com/example/francisco/androidejemplo/ActividadTres.kt
+++ app/src/main/java/com/example/francisco/androidejemplo/ActividadTres.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 
 import kotlinx.android.synthetic.main.activity_actividad_tres.*
@@ -15,6 +16,10 @@ class ActividadTres : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_tres)
         setSupportActionBar(toolbar)
+
+        val valorACompartir = intent.getStringExtra(Intent.EXTRA_TEXT)
+        Log.i("etiqueta", "valor a compartir: $valorACompartir")
+        text_view_compartir.text = valorACompartir
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
